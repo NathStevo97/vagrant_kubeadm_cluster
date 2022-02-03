8eaaac5ad57c7fb31f430477b9f73f41f853a04c
NathStevo97 <natstephenson15@gmail.com>
2022-02-03 21:08:14 +0000

    VMWare Kubeadm Kubernetes Cluster

---
+++ ubuntu/vagrant/setup-hosts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-bionic entry
+sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.169.190.11  master-1
+192.169.190.12  master-2
+192.169.190.21  worker-1
+192.169.190.22  worker-2
+192.169.190.30  lb
+EOF
